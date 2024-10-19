324fe23c573cba5ed8aa2065b3dea0c5ccbd5ade
Peter Minin <142728554+Arxpetro@users.noreply.github.com>
2024-10-19 21:01:31 +0200

    Update script.js

--- JS_22/Homework/script.js
+++ JS_22/Homework/script.js
@@ -3,9 +3,9 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 600;
-canvas.height = 600;
-canvas.padding = "auto"
-const XYoffset = 150;
+canvas.height = 1000;
+
+const XYoffset = 140;
 for (let i = 0; i < 10; i++) {
     ctx.strokeStyle = "green";
     ctx.lineWidth = 3;
@@ -37,12 +37,41 @@ for (let i = 0; i < 10; i++) {
     ctx.strokeRect(XYoffset, XYoffset+220+10*i, 100-i*10, 100-i*10);
 }
 
+//2
+const arr = [
+    {
+        title: 'javascript',
+        amount: 150000
+    },
+    {
+        title: 'golang',
+        amount: 130000
+    },
+    {
+        title: 'python',
+        amount: 140000
+    },
+    
+    {
+        title: 'java',
+        amount: 120000
+    }
+];
 
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
 
+ctx.font = "20px serif";
 
-
-
-
+for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    ctx.fillStyle = `rgb(${i * 50},${255 - i * 50},255)`;
+    // console.log(Math.floor(Math.random()*255));
+    
+    ctx.fillRect((i+1)*100, 700,100, -elem.amount/1000);
+    ctx.strokeText(elem.title,5+(i+1)*100, -10+700 );
+}
+ctx.strokeRect(100, 500, 400, 200);
 
 
 
